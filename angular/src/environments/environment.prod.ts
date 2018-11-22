7822fc8b28aba55adc3e2b069704447c2cabea2b
t.fiedler <thfied+git@gmail.com>
2018-11-22 09:51:25 +0100

    Change url strategy for client
    + Use `#` location strategy for client side
    routing so that the `now.sh` backend is able
    to route to the callback url for OAuth

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   base_url: 'https://peaceful-harbor-82034.herokuapp.com/api',
-  redirect_url: 'https://dist-13vdwv384.now.sh/authorize'
+  redirect_url: 'https://route-printer.now.sh/#/authorize'
 };
--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   base_url: 'http://localhost:3000/api',
-  redirect_url: 'http://localhost:4200/authorize'
+  redirect_url: 'http://localhost:4200/#/authorize'
 };
 
 /*
